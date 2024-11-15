d78d5f7086d1d0fb27a9252fc41b8e5cc91ec513
BARBOSAOJ <pbarboje@myuax.com>
2024-11-15 19:17:30 +0100

    PP

---
+++ src/main/java/com/jurassicpark/jurassicparkworld/JurassicParkWorldApplication.java
@@ -0,0 +1,13 @@
+package com.jurassicpark.jurassicparkworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurassicParkWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurassicParkWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jurassicpark/jurassicparkworld/JurassicParkWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jurassicpark.jurassicparkworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurassicParkWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
