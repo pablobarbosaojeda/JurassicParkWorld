3ebfd28e691ebf8f2cf2d66149b07276124dc567
BARBOSAOJ <pbarboje@myuax.com>
2024-11-16 11:46:33 +0100

    Login and SecurityConfig (Adrian)

---
+++ src/main/java/com/jurassicpark/jurassicparkworld/config/LoginConfig.java
@@ -0,0 +1,39 @@
+package com.jurassicpark.jurassicparkworld.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableWebSecurity
+public class LoginConfig {
+    @Bean
+    public SecurityFilterChain seguridadAccesos(HttpSecurity http) throws Exception{
+        http.authorizeRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+    @Bean
+    public UserDetailsService seguridadUsuarios(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("Admin")
+                .password(passwordEncoder().encode("1234"))
+                .roles("ADMIN")
+                .build());
+        return manager;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
